fe9e60f5cbd882dfa34a633e686ed7972a945e3b
Aditya Nahak <adityanahak295@gmail.com>
2026-07-22 23:10:57 +0530

    Done And Dusted

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -1,53 +1,62 @@
-import User from "../models/User.js"
-import { Webhook } from "svix"
+import User from "../models/User.js";
+import { Webhook } from "svix";
 
-const clerkWebhooks=async(req,res)=>{
-    try{
-      // Create A Svix Instance With Clerk Webhook Secret
-      const whook=new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+const clerkWebhooks = async (req, res) => {
+  try {
+    console.log("Webhook received:", req.body.type);
 
-      // Getting Headers
-      const headers={
-        "svix-id":req.headers["svix-id"],
-        "svix-timestamp":req.headers["svix-timestamp"],
-        "svix-signature":req.headers["svix-signature"],
-      };
+    // Create A Svix Instance With Clerk Webhook Secret
+    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
-      // Verifying Headers
-      await whook.verify(JSON.stringify(req.body),headers)
+    // Getting Headers
+    const headers = {
+      "svix-id": req.headers["svix-id"],
+      "svix-timestamp": req.headers["svix-timestamp"],
+      "svix-signature": req.headers["svix-signature"],
+    };
 
-      // Getting Data From req Body
-      const {data,type}=req.body
+    // Verifying Headers
+    await whook.verify(JSON.stringify(req.body), headers);
 
-      const userData={
-        _id:data.id,
-        email:data.email_addresses[0].email_addresses,
-        ussername:data.first_name + " " + data.last_name,
-        image:data.image_url,
+    // Getting Data From req Body
+    const { data, type } = req.body;
+
+    const userData = {
+      _id: data.id,
+      email: data.email_addresses[0].email_address,
+      username: data.first_name + " " + data.last_name,
+      image: data.image_url,
+    };
+    console.log("User data to save:", userData);
+
+    // Switch Cases For Different Events
+    switch (type) {
+      case "user.created": {
+        const createdUser = await User.create(userData);
+        console.log("User created:", createdUser);
+        break;
       }
-      // Switch Cases For Different Events
-      switch(type){
-        case "user.created":{
-          await User.create(userData)
-          break
-        }
-        case "user.updated":{
-          await User.findByIdAndUpdate(data.id,userData)
-          break
-        }
-        case "user.deleted":{
-          await User.findByIdAndDelete(data.id)
-          break
-        }
-        default:
-          break;
+      case "user.updated": {
+        const updatedUser = await User.findByIdAndUpdate(data.id, userData, {
+          new: true,
+        });
+        console.log("User updated:", updatedUser);
+        break;
       }
-      res.json({success:true,message:"Webhook Received"})
-
-    } catch(err){
-      console.log(error.message);
-      res.json({success:false,message:error.message});
+      case "user.deleted": {
+        await User.findByIdAndDelete(data.id);
+        console.log("User deleted:", data.id);
+        break;
+      }
+      default:
+        console.log("Unknown event type:", type);
+        break;
     }
-}
+    res.json({ success: true, message: "Webhook Received" });
+  } catch (err) {
+    console.error("Webhook error:", err.message);
+    res.status(400).json({ success: false, message: err.message });
+  }
+};
 
 export default clerkWebhooks;
--- server/server.js
+++ server/server.js
@@ -1,28 +1,28 @@
-import express from "express"
+import express from "express";
 import "dotenv/config";
 import cors from "cors";
-import { clerkMiddleware } from '@clerk/express'
+import { clerkMiddleware } from "@clerk/express";
 import connectDB from "./config/db.js";
 import clerkWebhooks from "./controllers/clerkWebhooks.js";
 
 connectDB();
 
-const app=express();
+const app = express();
 
-app.use(cors()) // it will enable Cross-origin resource sharing Means connect backend with
-app.use(express.json())
-app.use(clerkMiddleware())
+app.use(cors()); // it will enable Cross-origin resource sharing Means connect backend with
+app.use(express.json());
 
-// API to listen clerk Webhooks
+// API to listen clerk Webhooks (BEFORE clerkMiddleware)
+app.post("/api/clerk", clerkWebhooks);
 
-app.use("/api/clerk",clerkWebhooks);
+app.use(clerkMiddleware());
 
-app.get('/',(req,res)=>{
+app.get("/", (req, res) => {
   res.send("Api Is Running Fine");
-})
+});
 
-const port=process.env.port || 3000;
+const port = process.env.port || 3000;
 
-app.listen(port,()=>{
-  console.log("Server running on port",port);
-})
+app.listen(port, () => {
+  console.log("Server running on port", port);
+});
